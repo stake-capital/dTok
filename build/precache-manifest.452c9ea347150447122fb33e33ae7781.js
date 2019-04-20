a0a7572858284ef03beae8da2c7e4c08ccf918e5
Matej Nemček <ybdaba@gmail.com>
2019-04-20 21:19:51 +0200

    commiting build dont do this at home

---
+++ build/precache-manifest.452c9ea347150447122fb33e33ae7781.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "252bbf457902fe7db623",
+    "url": "/static/css/main.8801de92.chunk.css"
+  },
+  {
+    "revision": "252bbf457902fe7db623",
+    "url": "/static/js/main.ac21aae3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4314ad411ef0a71a3b9a",
+    "url": "/static/css/2.4428b864.chunk.css"
+  },
+  {
+    "revision": "4314ad411ef0a71a3b9a",
+    "url": "/static/js/2.9d142dde.chunk.js"
+  },
+  {
+    "revision": "e2983bb01d81943642bf94b46743ce29",
+    "url": "/static/media/bufficorn.e2983bb0.png"
+  },
+  {
+    "revision": "ed8e1f103e446cd87682e71699d71fbb",
+    "url": "/static/media/emojicoin.ed8e1f10.png"
+  },
+  {
+    "revision": "f0ff9cc44e4e0a6b7551647a6cc99134",
+    "url": "/static/media/cypherpunk.f0ff9cc4.png"
+  },
+  {
+    "revision": "5b15a8c6575231a6683cc8982faab6eb",
+    "url": "/static/media/dai.5b15a8c6.jpg"
+  },
+  {
+    "revision": "17fb2141e1acbb4caf3004515da0a755",
+    "url": "/static/media/burnerloader.17fb2141.gif"
+  },
+  {
+    "revision": "f636a29165bc40ac68324d3142aba740",
+    "url": "/static/media/burnerwallet.f636a291.png"
+  },
+  {
+    "revision": "58196683b28e9930b35c993d1ae20ff6",
+    "url": "/static/media/customRPCHint.58196683.png"
+  },
+  {
+    "revision": "76e57817ad4d57d2c50b9f01d16c0cb4",
+    "url": "/static/media/localeth.76e57817.png"
+  },
+  {
+    "revision": "a35e1352acf652121cf21ddc861478d1",
+    "url": "/index.html"
+  }
+];
