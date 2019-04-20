d66639d3062cb9478c83e211cad77be52ab86584
Matej Nemček <ybdaba@gmail.com>
2019-04-20 14:37:59 +0200

    commeting out emojis in balance exchange and vendor in Admin

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const EthCrypto = require('eth-crypto');
 //const POA_XDAI_NODE = "https://dai-b.poa.network"
 const POA_XDAI_NODE = "https://dai.poa.network"
 
-let emojis = ["🔥","❤️","🎉","🙏","🍑","💪","💩","👌"]
+let emojis = ["🔥","❤️"]
 
 let XDAI_PROVIDER = POA_XDAI_NODE
 
